c9b271db5033770e941c3710676b108b4738e5aa
Francisco Garache <franciscogarach3@gmail.com>
2022-05-26 14:42:06 +0000

    w

--- main.swift
+++ main.swift
@@ -36,3 +36,16 @@ let sara = Programmer(name: "sara", age: 12, languages: [.kotlin])
 print("")
 
 frank.code()
+
+
+final class APIClient {
+    func getSlackConversations(completionBlock: @escaping (ConversationsDataModel) -> ()) {
+        let url = URL(string: "https://slack.com/api/conversations.list?token=xoxb-1314965010803-1498101718961-mXey0PS6v41QfztMvJTJhQKb")!
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            let dataModel = try! JSONDecoder().decode(ConversationsDataModel.self, from: data!)
+            completionBlock(dataModel)
+        }
+        task.resume()
+    }
+}
